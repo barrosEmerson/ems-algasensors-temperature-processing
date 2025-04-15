e2902e50fa838f3d5fc25be6ab66e4f70ddc00f5
Emerson Barros <emerson.barrostech@hotmail.com>
2025-04-15 20:22:41 -0300

    first commit

---
+++ src/main/java/com/barrostech/algasensors/tempature/processing/TempatureProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.barrostech.algasensors.tempature.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempatureProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempatureProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barrostech/algasensors/tempature/processing/TempatureProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barrostech.algasensors.tempature.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempatureProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
